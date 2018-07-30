bc1ace35df4792700b0da371f2b8844fb8277101
Cosmin Stroe <cosmin@mailbox.org>
2018-07-30 17:39:20 -0500

    More docs.

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # checklister
 
 A backend service for managing checklists.
+
+## Usage
+
+Running:
+
+```
+docker-compose down                && \
+docker-compose up -d db            && \
+sleep 10                           && \
+./gradlew clean build              && \
+docker-compose down                && \
+docker-compose up -d --build       && \
+docker-compose logs -f checklister
+```
+
+Publishing:
+```
+
--- src/main/java/cloud/cosmin/checklister/rest/ListController.java
+++ src/main/java/cloud/cosmin/checklister/rest/ListController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -24,8 +23,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
-import static java.lang.String.format;
 
 @RestController
 public class ListController {
