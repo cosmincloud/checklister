0628f5adde10a30f22c2483c700e4f1f24bf4f95
Cosmin Stroe <cstroe@gmail.com>
2018-12-14 10:47:56 -0600

    Add Swagger UI.

--- src/main/kotlin/cloud/cosmin/checklister/config/SwaggerConfig.kt
+++ src/main/kotlin/cloud/cosmin/checklister/config/SwaggerConfig.kt
@@ -1,4 +1,21 @@
 package cloud.cosmin.checklister.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.RequestHandlerSelectors.basePackage
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
 class SwaggerConfig {
+    @Bean
+    fun checklisterApi(): Docket {
+        val docket = Docket(DocumentationType.SWAGGER_2);
+        docket.select()
+                .apis(basePackage("cloud.cosmin.checklister.rest"))
+                .build()
+        return docket
+    }
 }
--- src/main/kotlin/cloud/cosmin/checklister/rest/IndexController.kt
+++ src/main/kotlin/cloud/cosmin/checklister/rest/IndexController.kt
@@ -2,11 +2,20 @@ package cloud.cosmin.checklister.rest
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.annotations.ApiIgnore
 
 @RestController
+@ApiIgnore
 class IndexController {
-    @RequestMapping(path = arrayOf("/"), produces = arrayOf("text/plain"))
+    @RequestMapping(path = arrayOf("/"), produces = arrayOf("text/html"))
     fun index(): String {
-        return "Hello!"
+        return """
+            <html><body>
+            <h1>Checklister</h1>
+            <p>
+                <a href="swagger-ui.html">Swagger UI</a>
+            </p>
+            </body></html>
+        """.trimIndent()
     }
 }
