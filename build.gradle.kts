e32812cfa3f469a9b7083c03c73426bd9792f368
Cosmin Stroe <cstroe@gmail.com>
2018-12-22 09:34:22 -0600

    Small fixes to build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
 
-// fixed name for boot JAR (referenced in Dockerfile)
+// unchanging name for boot JAR (referenced in Dockerfile)
 val bootJar: BootJar by tasks
 bootJar.archiveName = "app.jar"
 
@@ -75,55 +75,58 @@ tasks.resolve {
 
 dependencies {
     // Kotlin
-    compile(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8"))
 
     // Spring Boot
-    compile(enforcedPlatform("org.springframework.boot:spring-boot-dependencies:2.1.1.RELEASE"))
+    implementation(enforcedPlatform("org.springframework.boot:spring-boot-dependencies:2.1.1.RELEASE"))
 
     // web
-    compile("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     // Swagger
-    compile("io.springfox:springfox-swagger2:2.9.2")
-    compile("io.springfox:springfox-swagger-ui:2.9.2")
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
 
     // database
-    compile("org.springframework.boot:spring-boot-starter-data-jpa")
-    compile("org.hibernate:hibernate-java8")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.hibernate:hibernate-java8")
     runtime("org.postgresql:postgresql:42.2.2")
-    compile("org.flywaydb:flyway-core:5.0.7")
+    implementation("org.flywaydb:flyway-core:5.0.7")
 
-    compile("org.bitbucket.cowwoc:requirements-core:4.0.4-RC")
+    implementation("org.bitbucket.cowwoc:requirements-core:4.0.4-RC")
 
     // Jackson
-    compile("com.fasterxml.jackson.core:jackson-databind:2.9.7")
-    compile("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.7")
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.7")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.7")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
 
     // because of Java 9+
     // https://stackoverflow.com/questions/43574426/how-to-resolve-java-lang-noclassdeffounderror-javax-xml-bind-jaxbexception-in-j#43574427
     // https://blog.sourced-bvba.be/article/2017/12/17/java9-spring/
-    compile("javax.xml.bind:jaxb-api:2.3.0")
-    compile("com.sun.xml.bind:jaxb-impl:2.3.0")
-    compile("org.glassfish.jaxb:jaxb-runtime:2.3.0")
-    compile("javax.activation:activation:1.1.1")
+    implementation("javax.xml.bind:jaxb-api:2.3.0")
+    implementation("com.sun.xml.bind:jaxb-impl:2.3.0")
+    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.0")
+    implementation("javax.activation:activation:1.1.1")
+
+    // JSON Patch
+    implementation("com.github.fge:json-patch:1.9")
 
     // dev tools
-    compile("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-devtools")
 
-    testCompile("org.springframework.boot:spring-boot-starter-test")
-    testCompile("org.springframework.boot:spring-boot-test-autoconfigure")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
 
     // JUnit 5
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
 
     // Support running JUnit 4 tests using JUnit 5
-    testCompileOnly("junit:junit:4.12")
+    testImplementation("junit:junit:4.12")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.3.2")
 
     // H2 in-memory database
-    testCompile("com.h2database:h2:1.4.197")
+    testImplementation("com.h2database:h2:1.4.197")
 
 //    integrationTestCompile("org.seleniumhq.selenium:selenium-java:3.13.0")
 //    integrationTestCompile("org.seleniumhq.selenium:selenium-remote-driver:3.13.0")
