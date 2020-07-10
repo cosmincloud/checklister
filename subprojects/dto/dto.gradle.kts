c0d9ad66c1793f3681710b7f27b3f23bede0990b
Cosmin Stroe <oncall@cosmin.cloud>
2020-07-10 01:21:34 -0500

    Use Kotest and Kotlin Serialization libraries

--- subprojects/dto/dto.gradle.kts
+++ subprojects/dto/dto.gradle.kts
@@ -1,17 +1,29 @@
 plugins {
     `java-library`
     kotlin("jvm") version "1.3.72"
+    // Kotlin Serialization: https://github.com/Kotlin/kotlinx.serialization
+    kotlin("plugin.serialization") version "1.3.72"
+    // https://github.com/kotest/kotest-gradle-plugin
+    id("io.kotlintest") version "1.1.1"
 }
 
 repositories {
     mavenCentral()
 }
 
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform { }
+}
+
 dependencies {
     // Kotlin
     implementation(kotlin("stdlib-jdk8"))
 
-    // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
+    // Kotlin Serialization: https://github.com/Kotlin/kotlinx.serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
+
+    // KoTest: https://github.com/kotest/kotest
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.1.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.1.1")
+    testImplementation("io.kotest:kotest-runner-console-jvm:4.1.1")
 }
